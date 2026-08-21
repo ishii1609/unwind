e5ded259aae53d0ca21ea5babd8bc20b74926c61
Isha Vohra <vohraish10@gmail.com>
2026-08-21 22:09:08 +0530

    added vercel link

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cookieParser =require ('cookie-parser')
 const app=express()
 const cors = require("cors");
 app.use(cors({
-  origin: "http://localhost:5173", // frontend ka exact URL
+  origin: "https://unwind-frontend.vercel.app/", // frontend ka exact URL
   credentials: true,
 }));app.use(express.json())
 
